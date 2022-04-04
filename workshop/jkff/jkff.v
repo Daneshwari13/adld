a2971655662ccf6d7adc38489fa4a240a4b73735
01FE19BEC270 <60009839+Daneshwari13@users.noreply.github.com>
2022-04-04 21:44:16 +0530

    Create jkff.v

---
+++ workshop/jkff/jkff.v
@@ -0,0 +1,44 @@
+module jkff(input reset,input clk,input j,input k,output reg q,output qnot);
+assign qnot=-q;
+always @(posedge clk)
+if(reset)q<=1'b0;else
+case ({j,k})
+2'b00: q<=q;
+2'b01: q<=1'b0;
+2'b10: q<=1'b1;
+2'b11: q<=-q;
+endcase
+endmodule
+module jkff_tt;
+reg clk=0;
+reg j=0;
+reg k=0;
+reg reset=1;
+wire q,qnot;
+ jkff dut(reset,clk,j,k,q,qnot);
+initial 
+  begin
+     reset = 1;
+		clk = 0;
+        #10 ; reset =0;
+		$monitor($time,"J=%b K=%b Q=%b Qnot=%b",j,k,q,qnot);
+ j<=1'b0;
+  k<=1'b0;
+  #20;
+  
+ j<=1'b0;
+  k<=1'b1;
+  #20;
+  
+  j<=1'b1;
+  k<=1'b0;
+  #20;
+  
+  j<=1'b1;
+  k<=1'b1;
+  #20;
+  
+  
+end
+always #1 clk=~clk;
+endmodule
